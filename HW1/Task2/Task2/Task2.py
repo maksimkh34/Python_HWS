949eeedb74d06164a4faca628732483d7585372f
Maksim <you@epic.twod@mail.ru>
2022-09-03 16:53:16 +0300

    Task 2

--- HW1/Task2/Task2/Task2.py
+++ HW1/Task2/Task2/Task2.py
@@ -1,1 +1,10 @@
-#
+#  !(x or y or z) == (!x and !y and !z)
+
+x = bool(input("Enter X: "))
+y = bool(input("Enter Y: "))
+z = bool(input("Enter Z: "))
+
+if not(x or y or z) == (not x and not y and not z):
+    print("True")
+else:
+    print("False")
