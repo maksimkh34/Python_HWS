1d7a2ece10ad7522ec8930eae81d6ebb3b05428e
Maksim <you@epic.twod@mail.ru>
2022-09-03 14:14:02 +0300

    Task 4

---
+++ HW1/Task4/Task4/Task4.py
@@ -0,0 +1,11 @@
+﻿num = int(input("Enter number: "))
+
+if num == 1:
+    print(" X -> (0; +∞) \n Y -> (0; +∞)")
+elif num == 2:
+    print(" X -> (-∞; 0) \n Y -> (0; +∞)")
+elif num == 3:
+    print(" X -> (-∞; 0) \n Y -> (-∞; 0)")
+elif num == 4:
+    print(" X -> (0; +∞) \n Y -> (-∞; 0)")
+else: print("err")
