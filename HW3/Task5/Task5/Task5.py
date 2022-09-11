aa6ac7ea328fcb6f2e55d38e4ecb89874a6338eb
Maksim <you@epic.twod@mail.ru>
2022-09-11 19:04:38 +0300

    Task 5

--- HW3/Task5/Task5/Task5.py
+++ HW3/Task5/Task5/Task5.py
@@ -1,1 +1,23 @@
+fib1 = 0
+fib2 = 1
 
+k = int(input("Enter k: "))
+
+plist = []
+
+for i in range(0, k-1):
+    fib1, fib2 = fib2, fib1 + fib2
+    plist.append(fib2)
+
+mlist = []
+
+for i in range(0, len(plist)):
+    j = len(plist) - i
+    if(i%2==0): mlist.append(-plist[j - 1])
+    else: mlist.append(plist[j - 1])
+
+templist = [1, 0, 1]
+
+list1 = mlist + templist + plist
+
+print(list1)
