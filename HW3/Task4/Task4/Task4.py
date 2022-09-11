e4cd337f9bc7b790c4bd16e8a8c50cd5f3132413
Maksim <you@epic.twod@mail.ru>
2022-09-11 12:33:51 +0300

    Tasks 2-4

--- HW3/Task4/Task4/Task4.py
+++ HW3/Task4/Task4/Task4.py
@@ -1,1 +1,14 @@
+num = int(input("Enter number: "))
 
+numlist = []
+
+while(num>0):
+    numlist.append(int(num%2))
+    num = int(num//2)
+
+strnum = ""
+
+for i in range(0, int(len(numlist))):
+    strnum += str(numlist[len(numlist) - 1 - i])
+
+print(strnum)
