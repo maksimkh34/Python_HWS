e7c626b26b4f7affe9475e3052685f99874ecc3f
Maksim <you@epic.twod@mail.ru>
2022-09-11 12:33:51 +0300

    Task 1

--- HW3/Task2/Task2/Task2.py
+++ HW3/Task2/Task2/Task2.py
@@ -1,1 +1,14 @@
+liststr = input("Enter list (nums separated by ' '): ")
+list1 = liststr.split(" ")
 
+lenlist = len(list1)
+
+list2 = []
+
+for i in range(0, lenlist//2):
+    list2.append(int(list1[i]) * int(list1[lenlist-1-i]))
+
+if(lenlist%2==1):
+    list2.append(int(list1[lenlist%2+1]) ** 2)
+
+print(list2)
--- HW3/Task3/Task3/Task3.py
+++ HW3/Task3/Task3/Task3.py
@@ -1,1 +1,20 @@
+strlist = input("Enter nums: ")
+templist = strlist.split(' ')
 
+list1 = []
+
+for i in range(0, len(templist)):
+    if(float(templist[i])%1!=0):
+        list1.append(float('0.' + templist[i].split('.')[1]))
+    else:
+        list1.append(0)
+
+max = list1[0]
+min = list1[0]
+for i in range(1, len(list1)):
+    if list1[i] > max:
+        max = list1[i]
+    if list1[i] < min:
+        min = list1[i]
+
+print(max-min)
