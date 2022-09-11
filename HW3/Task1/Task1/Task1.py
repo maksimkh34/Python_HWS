4a5d579b4f87dd01961c1771b37d950bf781cbdb
Maksim <you@epic.twod@mail.ru>
2022-09-11 11:40:58 +0300

    Task 1

--- HW3/Task1/Task1/Task1.py
+++ HW3/Task1/Task1/Task1.py
@@ -1,1 +1,9 @@
+liststr = input("Enter list (nums separated by ' '): ")
+list1 = liststr.split(" ")
 
+sum = 0
+
+for i in range(1, len(list1), 2):
+    sum += int(list1[i])
+
+print(f"Sum is: {sum}")
